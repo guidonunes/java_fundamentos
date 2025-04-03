39cc186da72f3b0cb57e7ad76100463bc51f4b84
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-03 15:54:30 -0300

    implement Spring config

--- learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/LearnSpringFrameworkApplication.java
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/LearnSpringFrameworkApplication.java
@@ -2,6 +2,7 @@ package com.in28minutes.spring.learnspringframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import com.in28minutes.spring.learnspringframework.game.GameRunner;
 import com.in28minutes.spring.learnspringframework.game.MarioGame;
@@ -13,11 +14,13 @@ import com.in28minutes.spring.learnspringframework.game.GamingConsole;
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		ConfigurableApplicationContext context 
+			= SpringApplication.run(LearnSpringFrameworkApplication.class, args);
 		//SuperContraGame game = new SuperContraGame();
 //		GamingConsole game = new PacManGame();
-		GamingConsole game = new MarioGame();
-		GameRunner runner = new GameRunner(game);
+		//GamingConsole game = new MarioGame();
+		//GameRunner runner = new GameRunner(game);
+		GameRunner runner = context.getBean(GameRunner.class);
 		runner.run();
 		
 	}
