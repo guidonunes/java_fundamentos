bb31d85ea596d5eda9fc9a8208a0283c3f0aba9b
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-03 15:05:32 -0300

    update class with interface

--- learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/game/GameRunner.java
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/game/GameRunner.java
@@ -2,9 +2,9 @@ package com.in28minutes.spring.learnspringframework.game;
 
 public class GameRunner {
 //	private MarioGame game;
-	private SuperContraGame game;
+	private GamingConsole game;
 
-	public GameRunner(SuperContraGame game) {
+	public GameRunner(GamingConsole game) {
 		this.game = game;
 	}
 
