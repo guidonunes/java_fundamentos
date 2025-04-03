4e1ba82585eaa8e932b22684a2a5d72b051aebb6
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-03 15:09:50 -0300

    Add class for the game

--- learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/LearnSpringFrameworkApplication.java
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/LearnSpringFrameworkApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.in28minutes.spring.learnspringframework.game.GameRunner;
 import com.in28minutes.spring.learnspringframework.game.MarioGame;
+import com.in28minutes.spring.learnspringframework.game.PacManGame;
 import com.in28minutes.spring.learnspringframework.game.SuperContraGame;
 
 @SpringBootApplication
@@ -13,7 +14,8 @@ public class LearnSpringFrameworkApplication {
 	public static void main(String[] args) {
 		//SpringApplication.run(LearnSpringFrameworkApplication.class, args);
 		//MarioGame game = new MarioGame();
-		SuperContraGame game = new SuperContraGame();
+		//SuperContraGame game = new SuperContraGame();
+		PacManGame game = new PacManGame();
 		GameRunner runner = new GameRunner(game);
 		runner.run();
 		
---
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/game/PacManGame.java
@@ -0,0 +1,20 @@
+package com.in28minutes.spring.learnspringframework.game;
+
+public class PacManGame implements GamingConsole {
+	public void up() {
+		System.out.println("PacMan up");
+	}
+	
+	public void down() {
+		System.out.println("PacMan down");
+	}
+	
+	public void left() {
+		System.out.println("PacMan left");
+	}
+	
+	public void right() {
+		System.out.println("PacMan right");
+	}
+
+}
