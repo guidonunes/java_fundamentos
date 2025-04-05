770b658350684cca1f4cce6161c1ed70c5d40514
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-05 11:17:03 -0300

    update tags and import statements

--- learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/enterprise/example/MyWebController.java
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/enterprise/example/MyWebController.java
@@ -3,18 +3,23 @@ package com.in28minutes.spring.learnspringframework.enterprise.example;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyWebController {
+	@Autowired
+	private BusinessService businessService;
 	
 	public long returnValueFromBusinessService() {
-		return 200;
+		return businessService.calculateSum();
 	}
 	
 }
 
+@Component
 class BusinessService {
+	@Autowired
 	private DataService dataService;
 	
 	public long calculateSum() {
@@ -26,6 +31,7 @@ class BusinessService {
 	}
 }
 
+@Component
 class DataService {
 	public List<Integer> getData() {
 		return Arrays.asList(10, 20, 30, 40);
