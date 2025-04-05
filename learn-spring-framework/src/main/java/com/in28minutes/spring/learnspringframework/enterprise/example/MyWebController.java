c6335f58cb53fe37c4df2c9dddd85c0a5d75f90e
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-05 11:12:52 -0300

    add example methods to test the class

--- learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/enterprise/example/MyWebController.java
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/enterprise/example/MyWebController.java
@@ -1,5 +1,8 @@
 package com.in28minutes.spring.learnspringframework.enterprise.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,5 +10,25 @@ public class MyWebController {
 	
 	public long returnValueFromBusinessService() {
 		return 200;
+	}
+	
+}
+
+class BusinessService {
+	private DataService dataService;
+	
+	public long calculateSum() {
+		List<Integer> data = dataService.getData();
+		return data.stream().reduce(Integer::sum).get();
+		
+		
+		
+	}
+}
+
+class DataService {
+	public List<Integer> getData() {
+		return Arrays.asList(10, 20, 30, 40);
+		
 	}
 }
