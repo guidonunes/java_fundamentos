103108044e79dd613c2c6bd039cb69b085078ad3
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-07 14:20:36 -0300

    add tag to setter method

--- learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/enterprise/example/business/BusinessService.java
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/enterprise/example/business/BusinessService.java
@@ -14,7 +14,7 @@ public class BusinessService {
 	
 	
 	
-
+	@Autowired
 	public void setDataService(DataService dataService) {
 		System.out.println("Setter injection");
 		this.dataService = dataService;
