4c36ac33c1502514e015d543e8a402bf06576cfa
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-07 14:57:37 -0300

    delete printing test

--- learn-spring-boot/src/main/java/com/in28minutes/springboot/learn_spring_boot/LearnSpringBootApplication.java
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learn_spring_boot/LearnSpringBootApplication.java
@@ -8,7 +8,7 @@ public class LearnSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringBootApplication.class, args);
-		System.out.println("Test");
+		
 	}
 
 }
