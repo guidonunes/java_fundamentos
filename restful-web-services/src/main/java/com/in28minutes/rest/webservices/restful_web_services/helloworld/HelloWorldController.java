0d91b04fe195baaa8db42346b5e7bc422d6fcc0a
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-12 11:15:48 -0300

    update annotation

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	
 	
-	@RequestMapping(method = RequestMethod.GET, path ="/hello-world")
+	@GetMapping(path ="/hello-world")
 	public String helloWorld() {
 		return "Hello World";
 	}
