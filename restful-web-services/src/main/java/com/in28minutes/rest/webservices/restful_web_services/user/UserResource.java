1d9e0a575bcc9314fd8f5fc448461a060f1e6678
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-12 15:25:09 -0300

    add class, constructor and method to retrieve all users

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -0,0 +1,23 @@
+package com.in28minutes.rest.webservices.restful_web_services.user;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	
+	private UserDaoService service;
+	
+	public UserResource(UserDaoService service) {
+		this.service = service;
+	}
+	
+	
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+	}
+	
+}
