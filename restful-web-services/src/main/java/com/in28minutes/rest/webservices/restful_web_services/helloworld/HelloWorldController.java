a19a188b9c5fff14d80b5f87ae051bb2914bf419
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-12 12:01:08 -0300

    update method return statement

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -22,7 +22,9 @@ public class HelloWorldController {
 	
 	@GetMapping(path ="/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
-		return new HelloWorldBean("Hello World");
+		return new HelloWorldBean(
+				String.format("Hello World, %s", name));
+		
 	}
 
 }
