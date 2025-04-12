b98073a1865a3114426f53d6018ee1108c8e7ac0
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-12 11:31:20 -0300

    change return type of the method

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -15,7 +15,7 @@ public class HelloWorldController {
 	
 	
 	@GetMapping(path ="/hello-world-bean")
-	public String helloWorldBean() {
+	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
 	}
 
