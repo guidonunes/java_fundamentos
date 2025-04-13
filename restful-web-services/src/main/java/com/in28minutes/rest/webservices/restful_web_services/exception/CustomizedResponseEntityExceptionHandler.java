96791f4ba26214d7cde88dfe1187ff452a56e5a7
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-13 11:27:13 -0300

    fix error handling implementation

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/exception/CustomizedResponseEntityExceptionHandler.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,15 +1,27 @@
 package com.in28minutes.rest.webservices.restful_web_services.exception;
 
+import java.time.LocalDate;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+
+@ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-	
-	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<Object> handleException(Exception ex, WebRequest request) throws Exception {
-		
-	}
-	
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDate.now(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
+    }
 }
+
+
--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/exception/ErrorDetails.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/exception/ErrorDetails.java
@@ -4,19 +4,19 @@ import java.time.LocalDate;
 
 
 public class ErrorDetails {
-	private LocalDate timeStamp;
+	private LocalDate timestamp;
 	private String message;
 	private String details;
 	
-	public ErrorDetails(LocalDate timeStamp, String message, String details) {
+	public ErrorDetails(LocalDate timestamp, String message, String details) {
 		super();
-		this.timeStamp = timeStamp;
+		this.timestamp = timestamp;
 		this.message = message;
 		this.details = details;
 	}
 
-	public LocalDate getTimeStamp() {
-		return timeStamp;
+	public LocalDate getTimestamp() {
+		return timestamp;
 	}
 
 	public String getMessage() {
