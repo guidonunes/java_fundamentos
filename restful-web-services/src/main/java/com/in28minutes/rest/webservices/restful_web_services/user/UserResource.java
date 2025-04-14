89becfdcbb8f3d29b9f1230fbaca6a72a1750397
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-14 15:44:46 -0300

    add delete mapping annotation

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,16 @@ public class UserResource {
 			throw new UserNotFoundException("id:" + id);
 		return user;
 	}
+	
+	@DeleteMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		User user = service.findOne(id);
+		
+		if(user == null )
+			throw new UserNotFoundException("id:" + id);
+		return user;
+	}
+	
 	
 	@PostMapping("/users")
 	public ResponseEntity<Object> createUser(@RequestBody User user) {
