5f7fa72006a514b4ab7ba94e9c7602905140e07a
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-15 17:14:03 -0300

    implement static filtering examples

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/filtering/SomeBean.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/filtering/SomeBean.java
@@ -1,7 +1,10 @@
 package com.in28minutes.rest.webservices.restful_web_services.filtering;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+
+@JsonIgnoreProperties("field1")
 public class SomeBean {
 	private String field1;
 	
