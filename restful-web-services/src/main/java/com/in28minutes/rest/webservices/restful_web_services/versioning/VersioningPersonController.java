e89bee74d91c3bd05d3c88fee76f21ca5495f383
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-15 15:31:44 -0300

    add example of versioning to v2

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -20,4 +20,9 @@ public class VersioningPersonController {
 	public PersonV1 getFirstVersionOfPersonRequestParameter() {
 		return new  PersonV1("Bob Charlton");
 	}
+	
+	@GetMapping(path = "person", params="version=2")
+	public PersonV2 getSecondVersionOfPersonRequestParameter() {
+		return new  PersonV2(new Name("Bob","Charlton"));
+	}
 }
