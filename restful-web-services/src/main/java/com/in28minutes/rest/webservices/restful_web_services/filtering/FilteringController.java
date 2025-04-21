f1f4823d7edd7c40167de08087e0359b9cf40647
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-21 15:17:53 -0300

    fix spacing

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/filtering/FilteringController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/filtering/FilteringController.java
@@ -17,11 +17,18 @@ public class FilteringController {
 	
 	@GetMapping("/filtering")
 	public MappingJacksonValue filtering() {
+		
 		SomeBean someBean = new SomeBean("value1", "value2", "value3");
+		
 		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+		
 		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field3");
-		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		
+		FilterProvider filters = 
+				new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		
 		mappingJacksonValue.setFilters(filters);
+		
 		return mappingJacksonValue;
 	}
 	
