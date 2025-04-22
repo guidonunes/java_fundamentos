fba3a600f7936908eacdba2d8bee60afacdcba1a
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-22 17:37:12 -0300

    add notes and class to fire queries

---
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,11 @@
+package com.in28minutes.springboot.learn_jpa_and_hibernate.course.jdbc;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+	
+	JdbcTemplate springJdbcTemplate;
+
+}
