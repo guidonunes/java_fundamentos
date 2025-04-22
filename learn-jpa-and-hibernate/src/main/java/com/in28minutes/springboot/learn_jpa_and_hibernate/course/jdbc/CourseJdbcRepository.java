a75e52518ff7a02629462517b548086683024fb5
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-22 17:41:26 -0300

    add method to fire queries

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -7,5 +7,16 @@ import org.springframework.stereotype.Repository;
 public class CourseJdbcRepository {
 	
 	JdbcTemplate springJdbcTemplate;
+	
+	private static String INSERT_QUERY =
+			"""
+				insert into course (id, name, author)
+				values(1, 'Learn AWS', 'in28minutes');
+			""";
+	
+	
+	public void insert() {
+		springJdbcTemplate.update(INSERT_QUERY);
+	}
 
 }
