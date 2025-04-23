bdb82ded27dd131a31da724777cc08b55694f1a8
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-23 15:31:35 -0300

    add constructors and getters

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/Course.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/Course.java
@@ -5,5 +5,41 @@ public class Course {
 	private String name;
 	private String author;
 	
+	public Course() {}
+	
+	public Course(long id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+
+	
+	
+	public long getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getAuthor() {
+		return author;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + ", author=" + author + "]";
+	}
+
+
+	
+
+
+	
 	
 }
