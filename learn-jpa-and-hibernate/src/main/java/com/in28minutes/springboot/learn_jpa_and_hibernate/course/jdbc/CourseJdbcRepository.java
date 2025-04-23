5ea34dc3f4b111e0d5491b77eb8a6965b10959b3
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-23 15:27:28 -0300

    create class and package

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,11 +1,15 @@
 package com.in28minutes.springboot.learn_jpa_and_hibernate.course.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.in28minutes.springboot.learn_jpa_and_hibernate.course.Course;
 
 @Repository
 public class CourseJdbcRepository {
 	
+	@Autowired
 	JdbcTemplate springJdbcTemplate;
 	
 	private static String INSERT_QUERY =
@@ -15,7 +19,7 @@ public class CourseJdbcRepository {
 			""";
 	
 	
-	public void insert() {
+	public void insert(Course course) {
 		springJdbcTemplate.update(INSERT_QUERY);
 	}
 
