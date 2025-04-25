12e51dad533c3429fb82823d26cc9829227f3b7e
Guilherme Nunes <guilherme.augd@gmail.com>
2025-04-25 16:46:34 -0300

    fix annotation bug

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jpa/CourseJpaRepository.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jpa/CourseJpaRepository.java
@@ -7,8 +7,10 @@ import com.in28minutes.springboot.learn_jpa_and_hibernate.course.Course;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 
 @Repository
+@Transactional
 public class CourseJpaRepository {
 	
 	@PersistenceContext
