617b839b79f906f08d9294d164daa4afe571ece4
Guilherme Nunes <guilherme.augd@gmail.com>
2025-05-05 14:24:48 -0300

    add interface and imports

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -1,2 +1,8 @@
 insert into user_details(id,birth_date,name)
 values(10001,current_date(), 'Sarah');
+
+insert into user_details(id,birth_date,name)
+values(10002,current_date(), 'Gui');
+
+insert into user_details(id,birth_date,name)
+values(10003,current_date(), 'Jessica');
