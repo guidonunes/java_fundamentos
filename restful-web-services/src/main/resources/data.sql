fe8e614b70c094ee74b374456c1d1bd9d4bdf678
Guilherme Nunes <guilherme.augd@gmail.com>
2025-05-06 18:22:01 -0300

    add data to table

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -6,3 +6,15 @@ values(10002,current_date(), 'Gui');
 
 insert into user_details(id,birth_date,name)
 values(10003,current_date(), 'Jessica');
+
+insert into post(id, description, user_id)
+values(20001,'Learn AWS', 10001);
+
+insert into post(id, description, user_id)
+values(20002,'Learn German', 10002);
+
+insert into post(id, description, user_id)
+values(20003,'Learn DevOps', 10001);
+
+insert into post(id, description, user_id)
+values(20004,'Learn React', 10002)
