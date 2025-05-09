943186d5771888be9390ceb79134ef58cf40681f
Guilherme Nunes <guilherme.augd@gmail.com>
2025-05-09 10:42:28 -0300

    add getters and setters

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/Post.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/Post.java
@@ -38,11 +38,21 @@ public class Post {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 	@Override
 	public String toString() {
 		return "Post [id=" + id + ", description=" + description + "]";
 	}
+
+
 	
 
 }
