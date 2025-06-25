ae0fd1dcb13dc190b56ab645cdff0b55761454b4
Guilherme Nunes <guilherme.augd@gmail.com>
2025-06-25 11:20:46 -0300

    add class and package

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/security/SpringSecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.in28minutes.rest.webservices.restful_web_services.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+}
