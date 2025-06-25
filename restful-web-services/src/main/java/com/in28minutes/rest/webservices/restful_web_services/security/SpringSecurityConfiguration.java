1bb868ae43e244a8587e76d38ea77f80a04188c1
Guilherme Nunes <guilherme.augd@gmail.com>
2025-06-25 11:31:16 -0300

    add method and organize imports

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/security/SpringSecurityConfiguration.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/security/SpringSecurityConfiguration.java
@@ -1,8 +1,17 @@
 package com.in28minutes.rest.webservices.restful_web_services.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
-
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		return http.build();
+		
+	}
 }
